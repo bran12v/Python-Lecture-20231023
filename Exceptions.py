94688a425d2ecf7e5188836a344eaa503e477aa1
bran12v <brandon.a.vanek@gmail.com>
2023-12-06 16:32:08 -0500

    adding afternoon content day 1

--- Exceptions.py
+++ Exceptions.py
@@ -10,14 +10,54 @@ try:
 except:
     print("This is the caught error")
 
+'''
+try
+
+except
+
+finally (optional) always runs regardless of everything else
+'''
 
 try:
     file = open("demo") # if this file doesnt exist
+    # 1/0
     # pull some info from the file
     # make some changes
     # print some stuff out
     # DO THINGS
-except:
+except FileNotFoundError:
     print("file does not exist")
     # print(1+1)
+# You can have multiple except blocks
+except Exception as e:
+    print(e)
+finally:
+    print("This always prints")
 
+def calculateSquareRoot(num):
+    # # Error Checking
+    # if type(num) is not int:
+    #     raise Exception("This number must be an Integer")
+    
+    # if num < 0:
+    #     raise Exception("This number must be positive")
+
+    return num ** 0.5 # 4^1/2 = 2
+    # Value of the function is the RETURN
+
+# sqrt = calculateSquareRoot(2) # = 2
+# print(sqrt)
+
+try:
+    # Casting - explicitly state what the type will be
+    val = int(input("Enter a number here: "))
+    result = calculateSquareRoot(val)
+    print(f"This is the square root of {val}: {result}")
+    val1 = int(input("Enter a number here: "))
+    val2 = int(input("Enter a number here: "))
+    result = val1 + val2
+    print(f"This is the sum of {val1} and {val2}: {result}")
+
+
+except Exception as e:
+    print(e)
